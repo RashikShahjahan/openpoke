9458580b5837da6ea970dcd5afb18c26942e1a52
RashikShahjahan <rashikshahjahan@protonmail.com>
2026-05-20 23:40:10 -0400

    add agent search service

--- server/services/execution/__init__.py
+++ server/services/execution/__init__.py
@@ -1,9 +1,13 @@
 """Execution agent support services."""
 
+from .agent_search import AgentSearchIndex, AgentSearchResult, get_agent_search_index
 from .log_store import ExecutionAgentLogStore, get_execution_agent_logs
 from .roster import AgentRoster, get_agent_roster
 
 __all__ = [
+    "AgentSearchIndex",
+    "AgentSearchResult",
+    "get_agent_search_index",
     "ExecutionAgentLogStore",
     "get_execution_agent_logs",
     "AgentRoster",
